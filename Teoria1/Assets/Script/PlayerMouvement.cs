55de693a0cb1265b7f0d99ae36a99bdb04c61ce8
Natalya <natalya_pilz@live.com>
2023-05-31 18:29:45 +0200

    Animação
    Animação do player corrida e parado, melhoria do cenário, camera segue o personagem

--- Teoria1/Assets/Script/PlayerMouvement.cs
+++ Teoria1/Assets/Script/PlayerMouvement.cs
@@ -1,29 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerMouvement : MonoBehaviour
 {
 
     private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private Animator anim;
+
+    private float dirX = 0f;
+    [SerializeField]private float moveSpeed = 7;
+    [SerializeField]private float jumpForce = 14;
 
     // Start is called before the first frame update
     void Start()
     {
         rb= GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        float dirtX = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(dirtX * 7f, rb.velocity.y);
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
         if (Input.GetKeyDown("space"))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(rb.velocity.x, 14f);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
-        
+
+        UpdateAnimationState();
+       
+    }
+
+    private void UpdateAnimationState()
+    {
+
+        if (dirX > 0)
+        {
+            anim.SetBool("running", true);
+            sprite.flipX = false;
+            
+        }
+
+        else if (dirX < 0)
+        {
+
+            anim.SetBool("running", true);
+            sprite.flipX = true;
+
+        }
+
+        else
+        {
+            anim.SetBool("running", false);
+        }
+
     }
 }
